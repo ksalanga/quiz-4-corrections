43871383b2fdd9732cc9ea7c601d1efbe17aa452
ksalanga <khsalanga@yahoo.com>
2019-01-20 23:48:44 -0500

    Create Odds.java

---
+++ Odds.java
@@ -0,0 +1,18 @@
+public class Odds {
+    
+    public String evensOnly(String original) {
+        if (original == null || original == "") {
+            return null;
+        }
+        
+        for(int i = 0; i < original.length(); i++) {
+            if (i % 2 != 0) {
+                String product2 = original.substring(original.charAt(i));
+                
+            }
+        } 
+        
+    }
+    
+    
+}
