64eec7fb8b45a8d6015a3859d1ce9d99573f5c0f
ksalanga <khsalanga@yahoo.com>
2019-01-20 23:26:02 -0500

    Update fluid.java

--- fluid.java
+++ fluid.java
@@ -1,23 +1,24 @@
+import java.util.Scanner;
+
 public class Fluid {
-	
-	public static void main(String args[]) {
-		System.out.print(fluidOunces(0));
-	}
-	
-	public static String fluidOunces(double ounces) {
-		if (ounces < 0) {
-			return null;
-		}
-		double gallon = ounces/128;
-		double quart = ounces/32;
-		double pint = ounces/16;
-		double cup = ounces/8;
-		
-		String message = gallon + " gallons, " + quart + " quarts, " + pint + " pints, and " + cup + " cups.";
-		return message; 
-	}
+    @SuppressWarnings("null")
+    public String fluidOunces(double ounces) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter the number of ounces: ");
+        ounces = s.nextDouble();
+        double W = 128/ounces;
+        double X = 32/ounces;
+        double Y = 16/ounces;
+        double Z = 8/ounces;
+        
+        if (ounces != Math.round(ounces)) {
+            ounces = (Double) null;
+        }
+        
+        String converted = W + "gallons, " + X + "quarts, " + Y + "pints, " + Z + "cups";
+        System.out.println(converted);
+        s.close();
+        return converted;
+    }
+
 }
-
-//ounces is guaranteed to be a double greater than or equal to 0. You method should calculate and return the number of gallons, quarts, pints, and cups than can be made from the original number of ounces. Fractional ounces should be ignored, but you cannot use typecasting.
-
-//Return a String in the following format: "W gallons, X quarts, Y pints, and Z cups." W, X, Y, and Z should represent the values your method computed.
